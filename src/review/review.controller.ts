bd869d88a61c6064d211108ad62ec93cfdc14289
Ilya Lykov <ilya.lykov.program@gmail.com>
2025-02-27 14:35:54 +0400

    add: create custom nest decorator

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,6 +15,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -36,8 +37,13 @@ export class ReviewController {
 		return deletedDoc;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {
+	async getByProduct(
+		@Param('productId') productId: string,
+		@UserEmail() email: string,
+	) {
+		console.log(email);
 		return this.reviewService.findByProductId(productId);
 	}
 }
